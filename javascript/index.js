6ce8ee04e64385444b51587c198cd50caab50a5b
AnttiK <anttiyleinen@hotmail.com>
2018-10-15 21:30:11 +0300

    Added buttons for saving name for user and entering to application.

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,17 @@
-
+function myFunction() {
+    var x = document.getElementById("frm1");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+        text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
+function myFunction1() {
+    var x = document.getElementById("divExample");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
