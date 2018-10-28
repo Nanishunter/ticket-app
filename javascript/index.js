ad8fd5df6744786d1566d5d25e30bd09586969a2
1701542-Saimia <anttiyleinen@hotmail.com>
2018-10-28 09:58:01 +0200

    modified layout

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,5 @@
-function myFunction() {
+// nimen tallentaminen näkyviin
+function saveName() {
     var x = document.getElementById("frm1");
     var text = "";
     var i;
@@ -7,7 +8,8 @@ function myFunction() {
     }
     document.getElementById("demo").innerHTML = text;
 }
-function myFunction1() {
+//sovelluksen avaus/sulkeminen
+function openApp() {
     var x = document.getElementById("divExample");
     if (x.style.display === "none") {
         x.style.display = "block";
@@ -15,3 +17,42 @@ function myFunction1() {
         x.style.display = "none";
     }
 }
+
+$(document).ready(function () {
+});
+
+    var price = prices;
+    //lipunhinnan laskeminenn
+$("#ageFree, #ageUnder, #ageOver, #mtk, #student, #soldier").on('change', function () {
+    var freeEntry = $("#ageFree").val();
+    var ageUnder = $("#ageUnder").val();
+    var ageOver = $("#ageOver").val();
+    var mtk = $("#mtk").val();
+    var student = $("#student").val();
+    var soldier = $("#soldier").val();
+
+    if(freeEntry == 1) {
+        $("#price").text(price.free.price + " " + "€");
+        $("#ageUnder, #ageOver, #mtk, #student, #soldier").prop('disabled', true);
+    } else if(ageUnder == 1) {
+        $("#price").text(price.normal.price * price.half.price + " " + "€");
+        $("#ageFree, #ageOver, #mtk, #student, #soldier").prop('disabled', true);
+    }else if(ageOver == 1) {
+        $("#price").text(price.normal.price * price.half.price + " " + "€");
+        $("#ageFree, #ageUnder, #mtk, #student, #soldier").prop('disabled', true);
+    }else if(mtk == 1) {
+        $("#price").text(price.normal.price * price.mtk.price + " " + "€");
+        $("#ageUnder, #ageOver, #ageFree, #soldier").prop('disabled', true);
+    }else if(student == 1) {
+        $("#price").text(price.normal.price * price.student.price + " " + "€");
+        $("#ageUnder, #ageOver, #ageFree, #soldier").prop('disabled', true);
+    } else if(soldier == 1) {
+        $("#price").text(price.normal.price * price.half.price + " " + "€");
+        $("#ageUnder, #ageOver, #mtk, #student, #ageFree").prop('disabled', true);
+    }
+     if(student == 1 && mtk == 1) {
+    $("#price").text(price.normal.price * price.student.price * price.mtk.price + " " + "€");
+     }
+
+     
+});
---
+++ javascript/prices.js
@@ -0,0 +1,32 @@
+// Json data
+  var prices = {
+    "prices":
+    
+    {
+        "Normaalihinta": "1",
+        "Alennusprosentti": "0"
+    },
+    
+    {
+        "Alle 7v"; "2"
+    }
+    {
+        "65v ja yli"; "3",
+        "Alennuskerroin"; "0,50"
+    }
+    {
+        "7-15v"; "4",
+        "Alennuskerroin"; "0,50"
+    }
+    {
+        "mtk-jäsen"; "5",
+        "Alennuskerroin"; "0,85"
+    }
+    {
+        "varusmies"; "6",
+        "Alennuskerroin"; "0,50"
+    }
+    {
+        "Opiskelija"; "7",
+        "Alennuskerroin"; "0,65"
+    };
